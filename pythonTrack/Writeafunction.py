ddaf5229e0c3c8b298b3c490e89c71ddcc9340f0
Natnael1234 <88520248+Natnael1234@users.noreply.github.com>
2022-11-18 18:26:46 -0600

    Update Writeafunction.py

--- pythonTrack/Writeafunction.py
+++ pythonTrack/Writeafunction.py
@@ -3,10 +3,15 @@ def is_leap(year):
     
     if (year % 4 == 0):
         leap = True
-    elif(year % 100 == 0):
-        leap = False
-    elif(year % 400 == 0):
-        leap = True
+        if(year % 100 == 0):
+            if(year % 400 == 0):
+                leap = True
+            else:
+                leap = False
+        
+            
+  
+   
         
     
     return leap
