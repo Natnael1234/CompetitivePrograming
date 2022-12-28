5067cbe7bdb007e1986c31a491af13e4b936a4ea
Natnael1234 <88520248+Natnael1234@users.noreply.github.com>
2022-12-28 03:16:49 -0600

    Create fibonaccinumber.py

---
+++ progress/fibonaccinumber.py
@@ -0,0 +1,14 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0: l == 0
+        if n == 1: l == 1
+        if n == 2:
+            l = 1 + 0
+            return l
+        elif n == 3:
+            l = 1 + 1
+            return l
+        elif n == 4:
+            l = 2 + 1
+            return l
+        return l
