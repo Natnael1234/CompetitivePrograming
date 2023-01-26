6eab9fca4a8d6caebcdc557b1d3535a80d936263
Natnael1234 <88520248+Natnael1234@users.noreply.github.com>
2023-01-26 07:31:01 +0300

    Update reversesubstring.py

--- progress/reversesubstring.py
+++ progress/reversesubstring.py
@@ -1,13 +1,13 @@
 class Solution:
     def reverseParentheses(self, s: str) -> str:
-        Stack = [] 
+        ans = [] 
         i = 0
         while i < len(s): 
             if s[i] == "(":
-                Stack.append(i)
+                ans.append(i)
             elif s[i] == ")":
-                if len(Stack) > 0:
-                    start = Stack.pop()
+                if len(ans) > 0:
+                    start = ans.pop()
                     print (start)
                     s = s[:start] + s[i-1: start: -1] + s[i+1:]
                     i -=2
