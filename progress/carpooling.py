81705d19e85e453973b8717fd469c475a4a63943
Natnael Mengistu Zerga <88520248+Natnael1234@users.noreply.github.com>
2023-02-06 21:24:16 +0300

    Create carpooling.py

---
+++ progress/carpooling.py
@@ -0,0 +1,14 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        
+        lst = []
+        for people,start, end in trips:
+            lst.append((start, people))
+            lst.append((end, -people))
+        lst.sort()
+        current = 0
+        for i in lst:
+            current += i[1]
+            if current > capacity:
+                return False
+        return True
