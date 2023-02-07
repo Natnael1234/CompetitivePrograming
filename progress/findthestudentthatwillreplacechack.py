0f54f9efe1597ec213fcc2b0b64e134acd412a40
Natnael Mengistu Zerga <88520248+Natnael1234@users.noreply.github.com>
2023-02-07 08:05:14 +0300

    Create findthestudentthatwillreplacechack.py

---
+++ progress/findthestudentthatwillreplacechack.py
@@ -0,0 +1,9 @@
+class Solution:
+    def chalkReplacer(self, chalk: List[int], k: int) -> int:
+        Sum = sum(chalk)
+        remainingSum = k % Sum
+        
+        for i in range(len(chalk)):
+            remainingSum -= chalk[i]
+            if remainingSum < 0:            
+                return i
